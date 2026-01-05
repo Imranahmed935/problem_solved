bbcf4f76e4b548dc5f47053f8f284891083b9371
Imranahmed935 <imrantahir9918@gmail.com>
2026-01-05 23:06:31 +0600

    1 problem done

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+
+const people = [
+  { name: "Imran", age: 22, gender: "male" },
+  { name: "Ayesha", age: 20, gender: "female" },
+  { name: "Rahim", age: 25, gender: "male" },
+  { name: "Fatema", age: 23, gender: "female" },
+  { name: "Karim", age: 28, gender: "male" }
+];
+
+function getMaleNames(persons) {
+  return persons
+    .filter(person => person.gender !== "female")
+    .map(person => person.name);
+}
+
+const result = getMaleNames(people);
+// console.log(result); // Output: ["Imran", "Rahim", "Karim"]
